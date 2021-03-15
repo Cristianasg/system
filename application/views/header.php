10203c099c68238828c32a4ce09938d4d5bac73d
Cristianasg <cristiangarciadasilva@gmail.com>
2021-03-15 19:01:31 -0300

    projeto inicial

---
+++ application/views/header.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Introdução ao Codeigniter + MVC </title>
+    </head>
+    <body>
+        <h1> <?php echo $title; ?></h1>
+        <em> &copy; 2021</em>
+    </body>
+</html>
