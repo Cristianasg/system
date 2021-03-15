4041eca86de82622b8107313a59ace466cc4185c
Cristianasg <cristiangarciadasilva@gmail.com>
2021-03-15 19:42:09 -0300

    template

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,5 +1,16 @@
 <?php
 
 class Pages extends CI_Controller{
-    public function view($page = 'home'){}
+    public function view($page = 'home')
+    
+    {
+        if(!file_exists(APPPATH . ' views/pages/' . $page . '.php'))
+        {
+            Show_404();
+        }
+        $data['title'] = ucfirst($page);
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages' . $page . '.php', $data);
+    }
 }
